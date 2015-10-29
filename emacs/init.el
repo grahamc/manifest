635c0b45db3e0f15e8e429bf34539e098dab712a
Graham Christensen <graham@grahamc.com>
2015-10-29 11:34:58 -0500

    Add osx cut/copy/paste support

--- emacs/init.el
+++ emacs/init.el
@@ -53,3 +53,22 @@
 (package-install 'python-mode)
 (package-install 'js)
 (package-install 'json-mode)
+
+;; osx copy and paste and cut
+(defun pbcopy ()
+  (interactive)
+  (call-process-region (point) (mark) "pbcopy")
+  (setq deactivate-mark t))
+
+(defun pbpaste ()
+  (interactive)
+  (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
+
+(defun pbcut ()
+  (interactive)
+  (pbcopy)
+  (delete-region (region-beginning) (region-end)))
+
+(global-set-key (kbd "C-c c") 'pbcopy)
+(global-set-key (kbd "C-c v") 'pbpaste)
+(global-set-key (kbd "C-c x") 'pbcut)
